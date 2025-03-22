ea909c5befee0f862669ce516e678ba3f1a5e48a
FrankSua15 <frankosua@gmail.com>
2025-03-22 18:41:15 -0500

    Se agregaron eventos de click a las tarjetas y se cambio la foto de perfil

---
+++ script.js
@@ -0,0 +1,21 @@
+// Evento de click a la tarjeta de la to do app
+document.getElementById("card_project_todoapp").addEventListener("click", function () {
+    window.open("https://github.com/FrankSua15/todoApp");
+});
+
+// Evento de click a la cada una de las tarjetas de redes sociales
+document.getElementById("button_github").addEventListener("click", function () {
+    window.open("https://github.com/FrankSua15");
+});
+
+document.getElementById("button_linkedin").addEventListener("click", function () {
+    window.open("https://www.linkedin.com/in/francisco-jos%C3%A9-suarez-herrera-31854a338/");
+});
+
+document.getElementById("button_youtube").addEventListener("click", function () {
+    window.open("https://www.youtube.com/@franciscojosesuarezherrera888");
+});
+
+document.getElementById("button_instagram").addEventListener("click", function () {
+    window.open("https://www.instagram.com/frankmfc17/");
+});
